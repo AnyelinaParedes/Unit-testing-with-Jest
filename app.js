72f321c6ba13bc72478d81ee38b28493b6d95a87
AnyelinaParedes <apg15194@hotmail.com>
2022-02-08 23:38:12 +0000

    Prueba Unitaria con Jest

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const fromEuroToDollar = function(valueInEuro){
+   
+    let valueInDollar = valueInEuro * 1.2;
+  
+    return valueInDollar;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const sum = (a,b) => {
+    return a + b
+}
+console.log(sum(7,3))
+module.exports = { sum, fromEuroToDollar }
